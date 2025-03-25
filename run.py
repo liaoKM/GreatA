54062fba563de3f8c83cd7a45bd8f60cc7f3ae36
kaiminliao <314863230@qq.com>
2025-03-26 00:43:22 +0800

    add parallel

--- run.py
+++ run.py
@@ -1,17 +1,21 @@
 import matplotlib.pyplot as plt
-
-
 import simple_backtrade
+from multiprocessing import Pool
 
-if __name__=='__main__':
-    #simple_backtrade.adata_request.update(2025,2025)
-    
-    start_time='2021-01-01'
-    end_time='2021-12-31'
-
+def task(year):
+    start_time='{}-01-01'.format(year)
+    end_time='{}-12-31'.format(year)
     simulator=simple_backtrade.backtrade.LocalSimulator(start_time,end_time)
     simulator.start()
-    
-    
 
-    pass
+
+if __name__=='__main__':
+    #debug
+    #2019-04-25
+    #600681
+    #10 +4shares +5yuan
+    #13.78->9.08
+    simple_backtrade.adata_request.update(2008,2016)
+    param_list=[2017,2018,2019,2020,2021,2022,2023,2024]
+    with Pool(len(param_list)) as p:
+        p.map(task,param_list)
